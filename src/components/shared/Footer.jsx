ed3c678badc70f87b018f2cd6802b2964c31af35
elora <108570316+eloraistired@users.noreply.github.com>
2023-11-12 18:15:10 +0000

    Added a loader

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -14,10 +14,10 @@ export const Footer = () => {
             <div className="absolute -inset-[1000%] bg-[conic-gradient(from_90deg_at_50%_50%,_theme(colors.green)_0%,_theme(colors.black)_50%,_theme(colors.green)_100%)] animate-spin"></div>
           </div>
           <span className="animate-border bg-[linear-gradient(110deg,_theme(colors.neutral.950),45%,_theme(colors.neutral.900),55%,_theme(colors.neutral.950))] bg-[length:200%_100%] rounded-full backdrop-blur-md uppercase flex items-center justify-center w-10 h-10">
-            <div className="w-full h-full wave"></div>
+            <div className="w-full h-full wave flex items-center justify-center text-green text-xs"></div>
           </span>
         </button>
-        <div className="uppercase text-[.65rem] text-green relative info transition-opacity duration-500">
+        <div className="uppercase text-[.65rem] text-green relative info transition-opacity duration-500 opacity-0">
           <div className="absolute inset-0 bg-[conic-gradient(from_90deg_at_50%_50%,_theme(colors.green/50%)_0%,_theme(colors.neutral.600)_50%,_theme(colors.green/50%)_100%)] -z-10"></div>
           <span className='block bg-black/70 backdrop-blur-lg px-6 py-2'>Click Anywhere To Start The Music</span>
         </div>
--- src/components/utils/Audio.js
+++ src/components/utils/Audio.js
@@ -7,30 +7,42 @@ export const AudioEffect = button => {
   if (initiated) return;
   initiated = true;
 
-  const manager = new AsyncLoader.manager();
-  const loader = new AsyncLoader.loader(manager).setResponseType('blob');
+  const info = button.parentNode.querySelector('.info');
+  const loader = new AsyncLoader.loader().setResponseType('blob');
 
-  loader.load('/assets/audio/01.mp3', function (audio) {
-    const controller = new AudioController(audio);
-    let control = false;
-    button.addEventListener('click', () => {
-      control ? (control = false) : (control = true);
-      controller.setVolume(control);
-    });
-    document.addEventListener(
-      'click',
-      () => {
-        controller.appendEqualizer(document.querySelector('.wave'));
-        if (controller.audio.paused) {
-          controller.play();
-          controller.setVolume(true);
-          const info = button.parentNode.querySelector('.info');
-          info.style.opacity = 0
-          info.addEventListener('transitionend', info.remove)
-          return;
-        }
-      },
-      { once: true }
-    );
-  });
+  loader.load(
+    '/assets/audio/01.mp3',
+    function (audio) {
+      const controller = new AudioController(audio);
+      info.style.opacity = 1;
+      let control = false;
+      button.addEventListener('click', () => {
+        control ? (control = false) : (control = true);
+        controller.setVolume(control);
+      });
+
+      document.addEventListener(
+        'click',
+        () => {
+          window.addEventListener('blur', () => controller.audio.pause());
+          window.addEventListener('focus', () => controller.audio.play());
+          controller.appendEqualizer(document.querySelector('.wave'));
+          if (controller.audio.paused) {
+            controller.play();
+            controller.setVolume(true);
+            button.querySelector('.wave').innerText = ''
+            info.style.opacity = 0;
+            info.addEventListener('transitionend', info.remove);
+            return;
+          }
+        },
+        { once: true }
+      );
+    },
+    function (xhr) {
+      if (xhr.lengthComputable) {
+        button.querySelector('.wave').innerText = ((xhr.loaded / xhr.total) * 100).toFixed(0);
+      }
+    }
+  );
 };
